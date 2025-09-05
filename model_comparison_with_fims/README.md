510fd65f87af4d871c84fc2cbe7704e0de0b3e3a
Bai-Li-NOAA <bai.li@noaa.gov>
2025-09-05 18:43:06 +0000

    add dockerfile and model comparison data

---
+++ model_comparison_with_fims/README.md
@@ -0,0 +1,58 @@
+# Model Comparison Project
+
+This project uses a simulation-estimation approach to evaluate the reliability of five age-structured assessment models developed by NOAA Fisheries: the Age Structured Assessment Program (ASAP), the Assessment Model for Alaska (AMAK), the Beaufort Assessment Model (BAM), the Fisheries Integrated Modeling System (FIMS), and Stock Synthesis (SS3).
+
+We provide two methods for replicating this analysis, allowing for a consistent and reproducible research environment.
+
+1. The Docker environment
+This method uses Docker to create a self-contained development environment with all required software pre-installed. The image includes R, specific system packages, all required R packages, and two web-based IDEs: RStudio Server and code-server (VS Code in the browser). This is the most reliable way to ensure the analysis runs correctly regardless of your local machine's configuration.
+
+1. The local {renv} Environment
+This method is for users who prefer to use their local R installation. It uses the {renv} package to restore a project-specific library of R packages from the renv.lock file, ensuring that the exact package versions used in the analysis are installed on your machine.
+TODO: set up {renv} and then copy renv.lock, .Rprofile, renv/active.R to Dockerfile
+
+## Docker quick start cheat sheet
+
+This guide provides the step-by-step commands to set up and run the development environment using Docker.
+
+1. Open a terminal and clone this project to your local machine, then navigate into the project directory.
+
+```Bash
+git clone NOAA-FIMS/fims-gcp-workstations
+cd fims-gcp-workstations/model_comparison_with_fims
+```
+
+2. Build and start the container
+
+```Bash
+docker compose up -d --build
+```
+
+3. Start `code-server` 
+```Bash
+docker exec -d r_dev_container code-server --bind-addr 0.0.0.0:8080 /workspaces/model-comparison-project
+```
+
+- Find password: `code-server` automatically generates a password. To find it, run this commend:
+
+```Bash
+docker exec r_dev_container cat /root/.config/code-server/config.yaml
+```
+
+- Copy the password from the output.
+
+- Install xdg-utils to open the web address in terminal.
+
+```Bash
+sudo apt update
+sudo apt install xdg-utils
+xdg-open http://localhost:8080
+```
+
+4. Install the R extension (ID: reditorsupport.r) from the Extensions view (Ctrl + Shift + X).
+
+5. When you are finished working, you can stop the container by running the following command from your project directory:
+
+```Bash
+docker compose down
+```
---
+++ model_comparison_with_fims/fims_model_comparison.R
@@ -0,0 +1,142 @@
+# Install required packages ---------------------------------------
+required_pkg <- c(
+  "remotes", "devtools", "here",
+  "rstudioapi", "gdata", "PBSadmb",
+  "stringr", "matrixcalc", "r4ss",
+  "readxl", "scales", "corrplot",
+  "glue", "parallel", "doParallel",
+  "RcppEigen", "TMB"
+)
+pkg_to_install <- required_pkg[!(required_pkg %in%
+  installed.packages()[, "Package"])]
+if (length(pkg_to_install)) install.packages(pkg_to_install)
+
+invisible(lapply(required_pkg, library, character.only = TRUE))
+
+remotes::install_github(repo = "cmlegault/ASAPplots")
+library(ASAPplots)
+remotes::install_github(repo = "NOAA-FIMS/Age_Structured_Stock_Assessment_Model_Comparison")
+library(ASSAMC)
+
+devtools::install_github(
+  "NOAA-FIMS/FIMS",
+  ref = "10d5103cecdf2e37b724eb15dbf66dfb146c472b"
+)
+library(FIMS)
+
+# Set up C1 (sigmaR = 0.4, om_sim_num = 160) ------------------------
+
+maindir <- file.path(here::here(), "data")
+model_input <- ASSAMC::save_initial_input()
+C1 <- ASSAMC::save_initial_input(
+  base_case = TRUE,
+  input_list = model_input,
+  maindir = maindir,
+  om_sim_num = 160,
+  keep_sim_num = 100,
+  figure_number = 10,
+  seed_num = 9924,
+  case_name = "C1"
+)
+
+ASSAMC::run_om(input_list = C1)
+
+# ASSAMC::run_em(
+#   em_names = c("AMAK", "ASAP", "BAM", "SS", "FIMS"),
+#   input_list = C1,
+#   em_input_filenames = data.frame(
+#     AMAK = "C0",
+#     ASAP = "C0",
+#     BAM = "C0",
+#     SS = "C1"
+#   )
+# )
+
+ASSAMC::run_em(
+  em_names = c("FIMS"),
+  input_list = C1
+)
+
+# ASSAMC::generate_plot(
+#   em_names = c("AMAK", "ASAP", "BAM", "SS", "FIMS"),
+#   plot_ncol = 2, plot_nrow = 3,
+#   plot_color = c("orange", "green", "red", "deepskyblue3", "purple"),
+#   input_list = C1
+# )
+
+# ASSAMC::generate_plot(
+#   em_names = c("FIMS"),
+#   plot_ncol = 2, plot_nrow = 3,
+#   plot_color = c("purple"),
+#   input_list = C1
+# )
+
+# Set up C2 (sigmaR = 0.4, om_sim_num = 1) ------------------------
+C2 <- save_initial_input(
+  base_case = TRUE,
+  input_list = model_input,
+  maindir = maindir,
+  om_sim_num = 2,
+  keep_sim_num = 2,
+  figure_number = 1,
+  seed_num = 9924,
+  case_name = "C2"
+)
+
+ASSAMC::run_om(input_list = C2)
+
+ASSAMC::run_em(
+  em_names = c("FIMS"),
+  input_list = C2
+)
+
+# ASSAMC::generate_plot(
+#   em_names = c("AMAK", "ASAP", "BAM", "SS", "FIMS"),
+#   plot_ncol = 2, plot_nrow = 3,
+#   plot_color = c("orange", "green", "red", "deepskyblue3", "purple"),
+#   input_list = C2
+# )
+
+ASSAMC::generate_plot(
+  em_names = c("FIMS"),
+  plot_ncol = 1, plot_nrow = 1,
+  plot_color = c("purple"),
+  input_list = C2
+)
+
+# Set up C3 (sigmaR = 0.4, om_sim_num = 2) ------------------------
+C3 <- save_initial_input(
+  base_case = TRUE,
+  input_list = model_input,
+  maindir = maindir,
+  om_sim_num = 3,
+  keep_sim_num = 3,
+  figure_number = 2,
+  seed_num = 9924,
+  case_name = "C3"
+)
+
+ASSAMC::run_om(input_list = C3)
+
+# ASSAMC::run_em(
+#   em_names = c("AMAK", "ASAP", "BAM", "SS", "FIMS"),
+#   input_list = C3,
+#   em_input_filenames = data.frame(
+#     AMAK = "C0",
+#     ASAP = "C0",
+#     BAM = "C0",
+#     SS = "C1"
+#   )
+# )
+
+ASSAMC::run_em(
+  em_names = c("FIMS"),
+  input_list = C3
+)
+
+# ASSAMC::generate_plot(
+#   em_names = c("AMAK", "ASAP", "BAM", "SS", "FIMS"),
+#   plot_ncol = 2, plot_nrow = 3,
+#   plot_color = c("orange", "green", "red", "deepskyblue3", "purple"),
+#   input_list = C3
+# )
